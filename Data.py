4d6f3bf07ebef8785d121b542fdfc14cb698bd63
Hazardax <59655061+Hazardax@users.noreply.github.com>
2020-04-19 11:47:05 +0300

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,7 @@
+# example: main_lib = {episode_code: [episode_text, info_buttons("list of
+# information for the buttons, len(info_buttons) / 2 is number of buttons"),
+# jump_episode_code("list of ways to delta episode_code, length == length info_buttons")]}
+
+episode_position = '1'
+
+main_lib = {}
